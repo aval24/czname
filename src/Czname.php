250f0ede598e2f1e45024b51f85147d7c7de5089
aval24 <dev@aval24.cz>
2024-06-11 13:19:54 +0700

    Update Czname.php

--- src/Czname.php
+++ src/Czname.php
@@ -5,32 +5,41 @@ namespace aval24\czname;
 class Czname
 {
     const PREFIX_PATTERNS = [
-		'/^Bc./i',
-		'/^BcA./i',
-		'/^Ing./i',
-		'/^Mgr./i',
-		'/^MgA./i',
-		'/^DiS./i',
-		'/^MBA/i',
-		'/^BBA/i',
-		'/^MPA/i',
-		'/^DBA/i',
-		'/^LL.M./i',
-		'/^PhDr./i',
-		'/^JUDr./i',
-		'/^RNDr./i',
-		'/^PharmDr./i',
-		'/^ThDr./i',
-		'/^ThLic./i',
-		'/^MUDr./i',
-		'/^MDDr./i',
-		'/^MVDr./i',
-		'/^Ph.D./i',
-		'/^CSc./i',
-		'/^DrSc./i',
-		'/^doc./i',
-		'/^prof./i',
+		'/^(Bc.)/i',
+		'/^(BcA.)/i',
+		'/^(Ing.)/i',
+		'/^(Mgr.)/i',
+		'/^(MgA.)/i',
+		'/^(DiS.)/i',
+		'/^(MBA)/i',
+		'/^(BBA)/i',
+		'/^(MPA)/i',
+		'/^(DBA)/i',
+		'/^(LL.M.)/i',
+		'/^(PhDr.)/i',
+		'/^(JUDr.)/i',
+		'/^(RNDr.)/i',
+		'/^(PharmDr.)/i',
+		'/^(ThDr.)/i',
+		'/^(ThLic.)/i',
+		'/^(MUDr.)/i',
+		'/^(MDDr.)/i',
+		'/^(MVDr.)/i',
+		'/^(Ph.D.)/i',
+		'/^(CSc.)/i',
+		'/^(DrSc.)/i',
+		'/^(doc.)/i',
+		'/^(prof.)/i',
     ];
+
+	const SUFFIX_PATTERNS = [
+		'/(Th.D.)$/i',
+		'/(Ph.D.)$/i',		
+		'/(CSc.)$/i',
+		'/(DrSc.)$/i',
+		'/(DSc.)$/i',
+		'/(akad.)$/i'
+	];
 
     const SOURCE_ALPHABET = ['á','é','ě','í','ý','ž','ú','ů','ř','š','ď','č','ť','Á','É','Ě','Í','Ý','Ž','Ú','Ů','Ř','Š','Ď','Č','Ť','Ä','ä','Ö','ö','Ü','ü','ň','ŕ','ź','ó','ñ','Ň','Ñ','Ŕ','Ź','Ó'];
     const RESULT_ALPHABET = ['a','e','e','i','y','z','u','u','r','s','d','c','t','A','E','E','I','Y','Z','U','U','R','S','D','C','T','A','a','O','o','U','u','n','r','z','o','n','N','N','R','Z','O'];
@@ -42,14 +51,15 @@ class Czname
             $name = mb_strtoupper($name, 'UTF-8');
 
         $str1 = trim($name);
-        $str2 = preg_replace(self::PREFIX_PATTERNS, "", $str1);
+        $str2 = preg_replace(array_merge(self::PREFIX_PATTERNS, self::SUFFIX_PATTERNS), "", $str1);
         $str3 = trim($str2);
         $str4 = str_replace([".", ","], "", $str3);
+		$str5 = trim($str4);
 
         if($transliterate)
-            $str4 = self::transliterate($str4, false);
+            $str5 = self::transliterate($str5, false);
 
-        return $str4;
+        return $str5;
     }
 
     public static function transliterate($source,  $toUpperCase = true)
